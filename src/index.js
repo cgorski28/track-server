abdb08419605b32cc5ea3c1e5ad4922331e3b24f
Cole Gorski <cole.gorski@accenture.com>
2022-02-06 09:09:48 -0600

    API server for auth and track routes

---
+++ src/index.js
@@ -0,0 +1,31 @@
+require('./models/User')
+require('./models/Track')
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const authRoutes = require('./routes/authRoutes')
+const trackRoutes = require('./routes/trackRoutes')
+const requireAuth = require('./middleware/requireAuth')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(authRoutes)
+app.use(trackRoutes)
+
+const mongoUri = 'mongodb+srv://dbAdmin:On5KHILHl1b1Jdmn@cluster0.vowz6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(mongoUri);
+mongoose.connection.on('connected', () => {
+    console.log("connected to mongo")
+})
+mongoose.connection.on('error', (err) => {
+    console.error('error connecting to mongo', err)
+})
+
+app.get('/', requireAuth, (req,res) => {
+    res.send(`Your email: ${req.user.email}`);
+})
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
